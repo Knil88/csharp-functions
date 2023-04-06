712e7881bac1224217bbd92a0acd8275f71c2336
Yari <narducciyarigmail.com>
2023-04-06 16:08:59 +0200

    completamento terza funzione

--- csharp-functions/Program.cs
+++ csharp-functions/Program.cs
@@ -4,22 +4,48 @@
 int[] numeri = { 1, 2, 3, 4, 5 };
 StampaArray(numeri);
 
-static void StampaArray(int[] arr)
+ void StampaArray(int[] arr)
 {
     foreach (int i in arr)
     {
         Console.Write(i + ", ");
-    }
+
+            }
 }
 
 //int Quadrato(int numero): che vi restituisca il quadrato del numero passato come parametro.
 
 int numero = 5;
-int potenza = Quadrato(numero);
-Console.WriteLine($"La potenza di {numero} è {potenza}");
 
+int quadro = Quadrato(numero);
 
-static int Quadrato(int numero)
+Console.WriteLine($"La potenza di {numero} è {quadro}");
+
+ int Quadrato(int numero)
 {
     return numero * numero;
 }
+
+//int[] ElevaArrayAlQuadrato(int[] array): che preso un array di numeri interi, restituisca un nuovo array con tutti gli elementi elevati quadrato. Attenzione: è importante restituire un nuovo array, e non modificare l’array come parametro della funzione!
+
+int[] arrayInteri = { 1, 2, 3, 4, 5 };
+
+ElevaArrayAlQuadrato(arrayInteri);
+int[] ElevaArrayAlQuadrato(int[] arr)
+{
+    int[] copiaArray = (int[])arrayInteri.Clone();
+    for (int i = 0; i < copiaArray.Length; i++)
+    {
+        copiaArray[i] = arr[i] * arr[i];
+
+        Console.WriteLine(arr[i]);
+        Console.WriteLine(copiaArray[i]);
+      
+
+    }
+    return copiaArray;
+
+    
+}
+
+
